7e61f60502c124262db47f8a307a336a126df167
nataemi <natalia.galda@coreservices.pl>
2021-02-13 13:35:33 +0100

    initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {InputSwitchModule} from 'primeng/inputswitch';
 
 import { AppComponent } from './app.component';
+import { TitleBannerComponent } from './title-banner/title-banner.component';
+import { UserInputComponent } from './user-input/user-input.component';
+import {FormsModule} from '@angular/forms';
+
+import {InputTextModule} from 'primeng/inputtext';
+import {ButtonModule} from 'primeng/button';
+import {CheckboxModule} from 'primeng/checkbox';
+import {RadioButtonModule} from 'primeng/radiobutton';
+import {InputTextareaModule} from 'primeng/inputtextarea';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TitleBannerComponent,
+    UserInputComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    InputTextModule,
+    CheckboxModule,
+    ButtonModule,
+    RadioButtonModule,
+    InputTextareaModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/user-input/user-input.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user-input',
+  templateUrl: './user-input.component.html',
+  styleUrls: ['./user-input.component.css']
+})
+export class UserInputComponent implements OnInit {
+
+  checked: boolean;
+  siblings: number;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
